a8e3ccf5856d314ec6e05fd0af5bb14f41ebd1da
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-21 23:34:59 -0400

    components and class added

--- src/README.md
+++ src/README.md
@@ -10,3 +10,17 @@ cd ANGULAR-HERO
 ```
 ng g module heroes --routing -m=app -d
 ```
+### Add heroes service and provide it to HeroesModule
+```
+ng g s heroes/heroes -m=heroes -d
+```
+### Generate heroes , hero-list and hero-detail components
+```
+ng g c heroes/heroes -d
+ng g c heroes/hero-list -d
+ng g c heroes/hero-detail -d
+```
+### Generate hero model
+```
+ng g class heroes/hero -d
+```
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroesService } from './heroes.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HeroesRoutingModule
   ],
-  declarations: []
+  declarations: [HeroesComponent, HeroListComponent, HeroDetailComponent],
+  providers: [HeroesService]
 })
 export class HeroesModule { }
